6b2886a46a89735f70f96a0a6ef3cb9eedcdfa81
JOOWAN HAN <joowan@seoultech.ac.kr>
2026-03-04 13:03:36 +0900

    feat: Text, Image, Icon 예제 코드 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,6 +1,10 @@
 package woowacourse.kanban.board
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,13 +12,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.default_planet
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-@Preview()
+@Preview(showBackground = true)
 fun App() {
 
-    CheckerScreen()
+    Column {
+        CheckerScreen()
+        TextExample()
+        ImageExample()
+        IconExample()
+    }
 }
 
 @Composable
@@ -35,4 +54,35 @@ fun CheckerView(checked: Boolean, check: () -> Unit) {
         )
         if (checked) Text(text = "체크됨!!!")
     }
+}
+
+@Composable
+fun TextExample() {
+    Text(
+        text = "Jetpack Compose Text 실습",      // 1. 표시할 문자열
+        color = Color.Blue,            // 2. 글자 색상
+        fontSize = 14.sp,              // 3. 글자 크기 (sp 단위 사용)
+        fontStyle = FontStyle.Italic,  // 4. 글자 형태 (기울임꼴)
+        fontWeight = FontWeight.Bold,   // 5. 글자 굵기 (굵게)
+    )
+}
+
+
+@Composable
+fun ImageExample() {
+    Image(
+        // R.drawable 대신 Res.drawable을 사용하며, 파라미터 이름은 'resource'이다.
+        painter = painterResource(resource = Res.drawable.default_planet),
+        contentDescription = "default_planet",
+        modifier = Modifier.size(48.dp),
+    )
+}
+
+@Composable
+fun IconExample() {
+    Image(
+        painter = rememberVectorPainter(image = Icons.Default.Favorite),
+        contentDescription = "좋아요 아이콘",
+        modifier = Modifier.size(48.dp),
+    )
 }
